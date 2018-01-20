e4e8f983f70e396d8d28bbcff162597f1e6e209c
Wally <wvdberg@gmail.com>
2018-01-20 22:10:14 +1100

    front end GUI improved

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Tic-tac-toe-game
 
-basic tic tac toe game using html css & js 
+basic tic tac toe game using html css & js
 nested arrays and nested loops
+
+https://github.com/wally50/Tic-tac-toe-game.git
--- game-tic-tac-toe/tttapp.js
+++ game-tic-tac-toe/tttapp.js
@@ -1,11 +1,7 @@
 console.log('Tic Tac Toe - ready!');
 
-var counterTurn = 0;
-var matchCount = 0;
-var play = 0;
-
-var player = [[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ] , [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]];
-
+var playerOneWinCount = 0;
+var playerTwoWinCount = 0;
 var winCombo = [
  [ 1 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 ],
  [ 0 , 0 , 0 , 1 , 1 , 1 , 0 , 0 , 0 ],
@@ -15,45 +11,116 @@ var winCombo = [
  [ 0 , 0 , 1 , 0 , 0 , 1 , 0 , 0 , 1 ],
  [ 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 1 ],
  [ 0 , 0 , 1 , 0 , 1 , 0 , 1 , 0 , 0 ]];
+ // deaults to reset
+ var eventClassString = "";
+ var counterTurn = 0;
+ var matchCount = 0;
+ var gameStarter = 0;
+ var endOfGameFlag = false;
+ var play = 0;
+ var player = [[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+               [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]];
 
 var selectedSquares = document.querySelectorAll('.box');
+var resetBoardInput = document.querySelector('.ipBoardReset');
+console.log(selectedSquares);
+
+var resetBoard = function () {
+  var x = document.querySelectorAll('.box');
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.backgroundImage = '';
+  }
+  document.querySelector('.oneWinner h2').classList.add('hidden');
+  document.querySelector('.twoWinner h2').classList.add('hidden');
+  document.querySelector('.draw').classList.add('hide');
+  endOfGameFlag = false;
+  if (gameStarter == 1) {
+    counterTurn = 1;
+  }
+  gameStarter = 0;
+};
+
+var resetCodeForNextGame = function () {
+  var x = document.querySelectorAll('.box');
+  for (var i = 0; i < x.length; i++) {
+    if (x[i].classList.contains("plyrOne")) {
+      x[i].classList.remove("plyrOne");
+    }
+    if (x[i].classList.contains("plyrTwo")) {
+      x[i].classList.remove("plyrTwo");
+    }
+  }
+  player = [[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ],
+            [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]];
+  eventClassString = '';
+  counterTurn = 0;
+  endOfGameFlag = true;
+  };
 
 var checkWin = function(){
-// loop thru and compare player square selection with wining combinations array.
+// loop thru and compare player square selections with wining combinations array.
+// notify of win or draw. Increment counter of each players wins.
   matchCount=0;
-  for (var ind = 0; ind <= winCombo.length-1; ind++) {
+  for (var ind = 0; ind <= winCombo.length-1; ind++)
+  {
     matchCount = 0;
-    for (var index = 0; index <= winCombo[ind].length-1; index++){
-      if(((winCombo[ind][index]) === 1) && (player[play][index] === winCombo[ind][index])){
+    for (var index = 0; index <= winCombo[ind].length-1; index++)
+    {
+      if(((winCombo[ind][index]) === 1) && (player[play][index] === winCombo[ind][index]))
+      {
         matchCount = matchCount +1;
-        if(matchCount === 3){
-          if (counterTurn === 1 || counterTurn === 3 || counterTurn === 5 || counterTurn === 7 || counterTurn === 9) {
+        console.log("Match Count: " + matchCount);
+        if(matchCount === 3)
+        {
+          if (counterTurn === 1 || counterTurn === 3 || counterTurn === 5 || counterTurn === 7 || counterTurn === 9)
+          {
+            playerOneWinCount = playerOneWinCount +1;
+            resetCodeForNextGame();
+            document.querySelector('.winCounterOne h5').textContent = playerOneWinCount;
             document.querySelector('.oneWinner h2').classList.remove('hidden');
           }
-          if (counterTurn === 2 || counterTurn === 4 || counterTurn === 6 || counterTurn === 8) {
+          if (counterTurn === 2 || counterTurn === 4 || counterTurn === 6 || counterTurn === 8 || counterTurn === 10)
+          {
+            playerTwoWinCount = playerTwoWinCount + 1;
+            resetCodeForNextGame();
+            document.querySelector('.winCounterTwo h5').textContent = playerTwoWinCount;
             document.querySelector('.twoWinner h2').classList.remove('hidden');
           }
         }
       }
     }
   }
+  if (( counterTurn === 9 && gameStarter == 1 && matchCount < 3) || ( counterTurn === 10 && gameStarter == 0 && matchCount < 3))
+    {
+      resetCodeForNextGame();
+      document.querySelector('.draw').classList.remove('hide');
+    }
 };
 
 var changeSquarePlayer1 = function (event) {
+  // Changes square selected. Convert to string, extract cell number identifier from string
+  // and convert to a number which becomes the index to insert a 1 into players array.
   play = 0; //required for checkWin
-  event.target.style.backgroundColor = "red";
+  event.target.style.backgroundSize = "90px 90px";
+  event.target.style.backgroundImage = "url('images/nought.png')";
   eventClassArray = event.target.classList;
   eventClassArray.add('plyrOne');
+  console.log(eventClassArray);
   if( eventClassArray.contains('plyrOne')){
-    eventClassString = eventClassArray.toString();
+    var eventClassString = eventClassArray.toString();
     var index = eventClassString.match(/\d+/g).map(Number);
     player[0].splice(index , 1 , 1 );
     checkWin();
   }
 };
 var changeSquarePlayer2 = function (event) {
+  // Changes square selected. Convert to string, extract cell number identifier from string
+  // and convert to a number which becomes the index to insert a 1 into players array.
   play = 1; //required for checkWin
-  event.target.style.backgroundColor = "green";
+  //event.target.style.backgroundColor = "#de0004";
+  event.target.style.backgroundSize = "90px 90px";
+  event.target.style.backgroundImage = "url('images/cross.png')";
+  //event.target.innerHTML='<img src="images/cross.png">';
   eventClassArray = event.target.classList;
   eventClassArray.add('plyrTwo');
   if( eventClassArray.contains('plyrTwo')){
@@ -66,16 +133,25 @@ var changeSquarePlayer2 = function (event) {
 
 function changeSquareClickHandler(event) {
   //  alert - no reclick on an already selected square and alternate turns.
-  if( event.target.classList.contains('plyrOne') || event.target.classList.contains('plyrTwo')){
-    alert ("Choose a different Square");
-  } else { counterTurn = counterTurn +1;
-      if ((counterTurn % 2) === 0) {
-        changeSquarePlayer2(event);
-      } else {changeSquarePlayer1 (event);
+  if (endOfGameFlag === false) {
+    if( event.target.classList.contains('plyrOne') || event.target.classList.contains('plyrTwo')){
+      alert ("Choose a different Square");
+    } else {
+        counterTurn = counterTurn +1;
+        if ((counterTurn % 2) === 0) {
+          changeSquarePlayer2(event);
+        } else {
+            if (counterTurn == 1) {
+              gameStarter = 1;
+            }
+            changeSquarePlayer1 (event);
+          }
       }
-    }
+  }
 }
 
-selectedSquares.forEach(function(elem){
-  elem.addEventListener('click', changeSquareClickHandler);
-});
+  selectedSquares.forEach(function(elem){
+    elem.addEventListener('click', changeSquareClickHandler);
+  });
+
+resetBoardInput.addEventListener('click', resetBoard);
